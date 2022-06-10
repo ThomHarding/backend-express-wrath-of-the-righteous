2c6d49235f2d5502c07f39231118e9d316a75cef
THOMAS HARDING <hardingt604@gmail.com>
2022-06-09 23:59:24 -0600

    add test for get all companions route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,23 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { companions } = require('../data/companions');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
+  it('/companions should return a list of companions', async () => {
+    const res = await request(app).get('/companions');
+    const expected = companions.map((companion) => {
+      return { id: companion.id, name: companion.name, class: companion.class };
+    });
+    expect(res.body).toEqual(expected);
+  });
 
   it('/companions/:id should return companion detail', async () => {
     const res = await request(app).get('/companions/1');
-    console.log(res);
     const camellia = {
       id: '1',
       name: 'Camellia',
